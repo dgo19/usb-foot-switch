b904eaa1d2220983405cc1d91a2bfc93daf66b92
Daniel Gottschalk <dgo@gmx.net>
2023-07-15 21:54:00 +0200

    adds pinmode 1 for neotrellis keypad with led buttons

--- usb-foot-switch.ino
+++ usb-foot-switch.ino
@@ -1,3 +1,7 @@
+// setup input mode
+// 0 = pin mode (one key per hw pin)
+// 1 = Adafruit NeoTrellis Keypad support
+#define PINMODE 0
 // Keyboard support (enabled=1, disabled=0)
 #define KEYBOARD 1
 // MIDI USB support (enabled=1, disabled=0)
@@ -12,6 +16,12 @@
 //#define DEBUG
 // define max elemets for keyconfig status arrays
 #define MAXELEMENTS 30
+
+#if PINMODE == 1
+#include "Adafruit_NeoTrellis.h"
+Adafruit_NeoTrellis trellis;
+#define INT_PIN 5
+#endif
 
 #if KEYBOARD == 1
 #include "Keyboard.h"
@@ -39,6 +49,29 @@ static char keyconfig[][5] = {{2, 'K', KEY_UP_ARROW, 0, 0},   // Switch Pin 5 Ke
                               {6, 'm', 0, 48, 64},            // Switch Pin 11 MIDI Note Channel 1, middle C, normal velocity
                               {7, 'c', 1, 20, 127}            // Switch Pin 12 MIDI Control Channel 2, Control 20, Value 127
                              };
+
+/* Config for NeoTrellis LED buttons (only usable in PINMODE 1)
+ *  Button { idle 0xRRGGBB, pressed 0xRRGGBB}
+ *  Colors: 0..255
+ */
+
+static uint32_t buttonled[][2] = {{0x7F0000, 0x000000}, // button  1
+                                  {0x007F00, 0x00FF00}, // button  2
+                                  {0x00007F, 0x0000FF}, // button  3
+                                  {0x000000, 0x0000FF}, // button  4
+                                  {0x000000, 0x0000FF}, // button  5
+                                  {0x000000, 0x0000FF}, // button  6
+                                  {0x000000, 0x0000FF}, // button  7
+                                  {0x000000, 0x0000FF}, // button  8
+                                  {0x000000, 0x0000FF}, // button  9
+                                  {0x000000, 0x0000FF}, // button 10
+                                  {0x000000, 0x0000FF}, // button 11
+                                  {0x000000, 0x0000FF}, // button 12
+                                  {0x000000, 0x0000FF}, // button 13
+                                  {0x000000, 0x0000FF}, // button 14
+                                  {0x000000, 0x0000FF}, // button 15
+                                  {0x000000, 0xFFFFFF}  // button 16
+                                 };
 
 #if MIDIUSB == 1
 // First parameter is the event type (0x09 = note on, 0x08 = note off).
@@ -271,6 +304,22 @@ int cmpfunc (const void * a, const void * b) {
   return ( *(char*)a - *(char*)b );
 }
 
+#if PINMODE == 1
+//define a callback for key presses
+TrellisCallback blink(keyEvent evt){
+  if(evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING) {
+    keyPressed(evt.bit.NUM); //on rising
+    trellis.pixels.setPixelColor(evt.bit.NUM, buttonled[evt.bit.NUM][1]);
+  }
+  else if(evt.bit.EDGE == SEESAW_KEYPAD_EDGE_FALLING) {
+    keyReleased(evt.bit.NUM); //off falling
+    trellis.pixels.setPixelColor(evt.bit.NUM, buttonled[evt.bit.NUM][0]);
+  }
+  trellis.pixels.show();
+  return 0;
+}
+#endif
+
 unsigned char switchstate[MAXELEMENTS];
 unsigned char inputpins[MAXELEMENTS];
 
@@ -282,7 +331,9 @@ void setup() {
   // set pins to input and enable internal pullup; init switch state; init inputpins
   for (count=0; count < MAXELEMENTS; count++)
   {
+#if PINMODE == 0
     pinMode(keyconfig[count][0], INPUT_PULLUP);
+#endif
     switchstate[count] = 1;
     inputpins[count] = 255;
   }
@@ -299,11 +350,35 @@ void setup() {
   // initialize Serial1 for MIDI, 31250 baud
   Serial1.begin(31250);
 #endif
+#if PINMODE == 1
+  pinMode(INT_PIN, INPUT);
+  if(!trellis.begin()){
+#ifdef DEBUG
+    Serial.println("could not start trellis");
+#endif
+    while(1) delay(1);
+  }
+#ifdef DEBUG
+  else{
+    Serial.println("trellis started");
+  }
+#endif
+  //activate all keys, set callbacks and init button led
+  for(int i=0; i<NEO_TRELLIS_NUM_KEYS; i++){
+    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_RISING);
+    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_FALLING);
+    trellis.registerCallback(i, blink);
+    trellis.pixels.setPixelColor(i, buttonled[i][0]);
+  }
+  delay(10);
+  trellis.pixels.show();
+#endif
 }
 
 void loop() {
   char count, keycount, currentswitchstate;
   char prevpin = 0;
+#if PINMODE == 0
   // loop for all configured keys
   for (count=0; count < MAXELEMENTS; count++)
   {
@@ -328,6 +403,12 @@ void loop() {
     }
     prevpin = inputpins[count];
   }
+#endif
+#if PINMODE == 1
+  if(!digitalRead(INT_PIN)){
+    trellis.read(false);
+  }
+#endif
   // delay in milliseconds
   delay(LOOPDELAY);
 }
